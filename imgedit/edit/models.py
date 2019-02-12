cae98292682d0fa82fa552d4abc2b5186cff8e0a
kasra <kakaee.kasra@gmail.com>
2019-02-12 14:05:19 +0330

    deleted the wrong version

--- imgedit/edit/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class aks(models.Model):
-    img = models.ImageField(default='', blank=True)
-    show = models.BooleanField(default=False)
